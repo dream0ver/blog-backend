686fe4d6a69a82961cc2321b6f1303cb69a71ba0
chaithanya <krishnachaithanya826@gmail.com>
2023-08-17 00:27:08 +0530

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const authRoutes = require("./routes/authRoutes")
 const blogRoutes = require("./routes/blogRoutes")
+const corsOptions = require("./util/corsOptions")
 const PORT = process.env.PORT
 const app = express()
+app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
 app.use("/api/auth/", authRoutes)
---
+++ util/corsOptions.js
@@ -0,0 +1,12 @@
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS Bye Bye."))
+    }
+  },
+  optionsSuccessStatus: 200
+}
+module.exports = corsOptions
